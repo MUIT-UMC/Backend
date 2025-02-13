a0bfcd36f362006c2672da43435699aea5f7998e
yhi9839 <yhi9839@gmail.com>
2025-02-13 18:18:16 +0900

    fix : 티켓 예매에 accountName 추가

--- src/main/java/muit/backend/s3/FilePath.java
+++ src/main/java/muit/backend/s3/FilePath.java
@@ -6,17 +6,21 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum FilePath {
+    AMATEUR_CASTING("amateur_casting"),
+    AMATEUR_NOTICE("amateur_notice"),
+    AMATEUR_SUMMARY("amateur_summary"),
     AMATEUR("amateur"),
     BLIND("blind"),
     FOUND("found"),
     LOST("lost"),
+    MUSICAL_ACTOR("music_actor"),
+    MUSICAL_NOTICE("music_notice"),
+    MUSICAL_POSTER("music_poster"),
+    MUSICAL_PREVIEW("music_preview"),
     REVIEW("review"),
+    Section("section"),
     SIGHT("sight"),
-    AMATEUR_CASTING("amateur_casting"),
-    AMATEUR_NOTICE("amateur_notice"),
-    AMATEUR_SUMMARY("amateur_summary"),
-    Theatre("theatre"),
-    Section("section");
+    Theatre("theatre");
 
     private final String path;
 }
